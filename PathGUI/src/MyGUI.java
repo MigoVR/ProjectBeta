216707abc57a6df9da0aac6d4b8268988c036037
MigoVR <hernandezmiguel0521@gmail.com>
2021-03-18 22:29:07 -0400

    created java proj for GUI

---
+++ PathGUI/src/MyGUI.java
@@ -0,0 +1,20 @@
+package src;
+
+import java.awt.*;
+
+public class MyGUI {
+
+	
+	
+	/*
+	 * Given x and y dimensions, create and display the GUI for tracking story text file. 
+	 */
+	private void initWindow() {
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
